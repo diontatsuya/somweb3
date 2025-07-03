0dce368203d7b42db5f5dc82639941819f332557
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-03 22:18:25 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { PrivyProvider } from '@privy-io/react-auth';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <PrivyProvider appId="cmcn6y46j00mnl40m3u5bee9v">
+    <App />
+  </PrivyProvider>
+);
